a550fe1a1d6eb3e92b87d3a1ce6492b8bfe3ac5d
cntx-gnewton <gnewton@cosnetix.com>
2023-05-16 21:04:16 -0500

    0.0.1 package

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,111 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('codeatlas.exportCodebase', () => {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            vscode.window.showErrorMessage('No workspace opened.');
+            return;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codeatlas" is now active!');
+        // This will use the first workspace folder as the root path
+        const rootPath = workspaceFolders[0].uri.fsPath;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codeatlas.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeAtlas!');
-	});
+        const outputPath = path.join(rootPath, 'codebase.txt');
+        const writeStream = fs.createWriteStream(outputPath);
 
-	context.subscriptions.push(disposable);
+        const config = vscode.workspace.getConfiguration('codeatlas');
+        const includePaths = config.get<string[]>('include', ["**/*"]).map(pattern => path.join(rootPath, pattern));
+        const excludeExtensions = config.get<string[]>('exclude', []);
+
+        includePaths.forEach(includePath => {
+            if (fs.existsSync(includePath)) {
+                traverseDirectory(includePath, '', writeStream, excludeExtensions);
+            }
+        });
+
+        writeStream.end();
+        vscode.window.showInformationMessage('Codebase exported successfully.');
+    });
+
+    let disposableMarkdown = vscode.commands.registerCommand('codeatlas.exportCodebaseToMarkdown', () => {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            vscode.window.showErrorMessage('No workspace opened.');
+            return;
+        }
+
+        const rootPath = workspaceFolders[0].uri.fsPath;
+
+        const outputPath = path.join(rootPath, 'codebase.md');
+        const writeStream = fs.createWriteStream(outputPath);
+
+        const config = vscode.workspace.getConfiguration('codeatlas');
+        const includePaths = config.get<string[]>('include', ["**/*"]).map(pattern => path.join(rootPath, pattern));
+        const excludeExtensions = config.get<string[]>('exclude', []);
+
+        includePaths.forEach(includePath => {
+            if (fs.existsSync(includePath)) {
+                traverseDirectoryMarkdown(includePath, '', writeStream, excludeExtensions);
+            }
+        });
+
+        writeStream.end();
+        vscode.window.showInformationMessage('Codebase exported to Markdown successfully.');
+    });
+
+    context.subscriptions.push(disposable, disposableMarkdown);
 }
 
-// This method is called when your extension is deactivated
+
+
+function traverseDirectory(dir: string, prefix: string, writeStream: fs.WriteStream, excludeExtensions: string[]) {
+    const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+    for (const entry of entries) {
+        const fullPath = path.join(dir, entry.name);
+        const newPrefix = path.join(prefix, entry.name);
+
+        if (entry.isDirectory()) {
+            writeStream.write(`\n${newPrefix}/\n`);
+            traverseDirectory(fullPath, newPrefix, writeStream, excludeExtensions);
+        } else {
+            if (excludeExtensions.some(ext => fullPath.endsWith(ext))) {
+                continue;
+            }
+
+            writeStream.write(`\n${newPrefix}\n`);
+            let content = fs.readFileSync(fullPath, 'utf-8');
+            content = content.replace(/[\r|\n|\r\n]+/g, '\n').replace(/[\u0085\u2028\u2029]/g, '');
+            writeStream.write(`${content}\n`);
+        }
+    }
+}
+
+function traverseDirectoryMarkdown(dir: string, prefix: string, writeStream: fs.WriteStream, excludeExtensions: string[]) {
+    const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+    for (const entry of entries) {
+        const fullPath = path.join(dir, entry.name);
+        const newPrefix = path.join(prefix, entry.name);
+
+        if (entry.isDirectory()) {
+            writeStream.write(`\n# ${newPrefix}/\n`);
+            traverseDirectoryMarkdown(fullPath, newPrefix, writeStream, excludeExtensions);
+        } else {
+            if (excludeExtensions.some(ext => fullPath.endsWith(ext))) {
+                continue;
+            }
+
+            writeStream.write(`\n## ${newPrefix}\n`);
+            let content = fs.readFileSync(fullPath, 'utf-8');
+            content = content.replace(/[\r|\n|\r\n]+/g, '\n').replace(/[\u0085\u2028\u2029]/g, '');
+            writeStream.write(`\n\`\`\`\n${content}\n\`\`\`\n`);
+        }
+    }
+}
+
+
 export function deactivate() {}
